5a572beb792234296a2def7340a70551480cdd00
LaiaRuizM <laia.ruizm@hotmail.com>
2023-02-14 10:06:49 +0100

    add JS, select, random numbers

--- js/main.js
+++ js/main.js
@@ -2,14 +2,38 @@
 
 const selectNumber = document.querySelector(".js-number");
 const selectButton = document.querySelector(".js-btn");
+const tipMessage = document.querySelector(".js-message");
+const letsTry = document.querySelector(".js-try");
 
+function getRandomNumber(max) {
+    const numberRandom = Math.ceil(Math.random() * max);
+    return numberRandom; 
+}  
 
+function useRandomNumber() {
+    const userNumber = selectNumber.value; 
+    const number = getRandomNumber(100);
+    if (userNumber > number) {
+        tipMessage.innerHTML = "Demasiado alto.";
+        if (userNumber > 100) {
+            tipMessage.innerHTML = "El número debe esta entre 1 y 100.";
+        }
+    } else if (userNumber < number) {
+        tipMessage.innerHTML = "Demasiado bajo.";
+        if (userNumber < 1) {
+            tipMessage.innerHTML = "El número debe esta entre 1 y 100.";
+        }
+    } else if (userNumber === number) {
+        tipMessage.innerHTML = "Has ganado campeona!!!";
+    } 
+     console.log(number);
+}
 
-
-
-
-
-
+// selectButton = 0;
+function reTry () {
+    selectButton += 1;
+    letsTry.innerHTML = selectButton.value;
+} 
 
 
 
@@ -17,8 +41,8 @@ const selectButton = document.querySelector(".js-btn");
 
 function handleClickButton(event) {
     event.preventDefault();
-
-
+    useRandomNumber();
+    reTry();
 }
 
 
