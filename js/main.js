7f40cf78a5e9a4428e0983319ea183968bbaa3da
LaiaRuizM <laia.ruizm@hotmail.com>
2023-02-14 09:04:53 +0100

    Guess the number - index.html

---
+++ js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const selectNumber = document.querySelector(".js-number");
+const selectButton = document.querySelector(".js-btn");
+
+
+
+
+
+
+
+
+
+
+
+
+
+function handleClickButton(event) {
+    event.preventDefault();
+
+
+}
+
+
+selectButton.addEventListener("click", handleClickButton);
