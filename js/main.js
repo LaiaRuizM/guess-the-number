528642534b6fbe92ab43abed29c46f786b06c9a9
LaiaRuizM <laia.ruizm@hotmail.com>
2023-02-20 16:37:51 +0100

    add improvements

--- js/main.js
+++ js/main.js
@@ -4,43 +4,52 @@ const selectNumber = document.querySelector(".js-number");
 const selectButton = document.querySelector(".js-btn");
 const tipMessage = document.querySelector(".js-message");
 const letsTry = document.querySelector(".js-try");
+const number = getRandomNumber(100);
 
+// Función del número aleatorio
 function getRandomNumber(max) {
-    const numberRandom = Math.ceil(Math.random() * max);
-    return numberRandom; 
+    return Math.ceil(Math.random() * max);
 }  
 
+// Función para modificar el texto de las pistas (condicionales) y no poner todo el rato el innerHTML
+function insertClue(msg) {
+  tipMessage.innerHTML = msg;
+};
+
 function useRandomNumber() {
-    const userNumber = selectNumber.value; 
-    const number = getRandomNumber(100);
-    if (userNumber > number) {
-        tipMessage.innerHTML = "Demasiado alto.";
-        if (userNumber > 100) {
-            tipMessage.innerHTML = "El número debe esta entre 1 y 100.";
-        }
+    const userNumber = parseInt(selectNumber.value); // parseInt -> para que no nos devuelva un string, sino un number
+    console.log(number);
+    if (userNumber === "") {
+        insertClue("¡Está vacío 👀! ");
+    // } else if (isNan(userNumber)) { // isNan sirve, por si en el INPUT hemos puesto type text y no type number e introducen texto
+    //     insertClue("Debes introducir un valor numérico :)");
+    } else if (userNumber < 1 || userNumber > 100) {
+            insertClue("El número debe esta entre 1 y 100.");
+    } else if (userNumber > number) {
+            insertClue("Demasiado alto.");
     } else if (userNumber < number) {
-        tipMessage.innerHTML = "Demasiado bajo.";
-        if (userNumber < 1) {
-            tipMessage.innerHTML = "El número debe esta entre 1 y 100.";
-        }
-    } else if (userNumber === number) {
-        tipMessage.innerHTML = "Has ganado campeona!!!";
+            insertClue("Demasiado bajo.");
+    } else { // userNumber === number
+        insertClue("Has ganado campeona!!!");
     } 
-     console.log(number);
 }
 
-const countAttemps = getRandomNumber(100);
-let attempts = 1;
+// const countAttemps = getRandomNumber(100);
+let attempts = 0;
 
+
+// Función contador -> incremento del contador, cada vez que se intenta adivinar el número
 function reTry() {
     const totalTries = attempts++;
     letsTry.innerHTML = `Número de intentos: ${totalTries}`;
 } 
 
+// Función manejadora del evento
 function handleClickButton(event) {
     event.preventDefault();
     useRandomNumber();
     reTry();
 }
 
+// Evento: click en button
 selectButton.addEventListener("click", handleClickButton);
